597b437c855523b3214bd957fe6c04a4d4bfee7d
kdw <dkrugg@mail.ru>
2026-07-20 00:36:41 +0300

    fastAPI init

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Старт программы!")
+# pip install -r requirements.txt
+# python.exe -m pip install --upgrade pip
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
