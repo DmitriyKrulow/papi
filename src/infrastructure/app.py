eb14391bbf3afbe54f64d4efd686b375665d00a6
kdw <dkrugg@mail.ru>
2026-07-21 22:32:07 +0300

    подключение к базе данных

--- src/infrastructure/app.py
+++ src/infrastructure/app.py
@@ -3,9 +3,23 @@
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+import psycopg2
+import os
+
+load_dotenv()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/db-check")
+def check_db_connection():
+    try:
+        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        conn.close()
+        return {"status": "connected", "database": "papiBD"}
+    except Exception as e:
+        return {"status": "disconnected", "error": str(e)}
 
 @app.get("/")
 def root():
